54c5ea4a7a3459e790dd187ef8b62d3e11eb6b31
Kaushal Tarpara <kaushaltarpara10702@gmail.com>
2023-05-29 15:00:48 +0530

    Using Spring MVC Create AddProduct and FindProductProject

--- Product-Manager/src/main/java/Main.java
+++ Product-Manager/src/main/java/Main.java
@@ -5,23 +5,23 @@ public class Main {
 
         ProductService service=new ProductService();
 
-//        Product p=new Product("Asus","Laptop","Brown",2000);
-//        service.addProduct(p);
-//        service.addProduct(new Product("Type C", "Cable", "Black Drawer", 2024));
-//        service.addProduct(new Product("Mac Studio", "Computer", "White Table", 2025));
-//        service.addProduct(new Product("Focusrite Mixer", "Audio System", "White Table", 2025));
-//        service.addProduct(new Product("Asus Vivobook", "Laptop", "Brown Drawer", 2021));
-//        service.addProduct(new Product("Asus Rog", "Laptop", "Black Table", 2021));
-//        service.addProduct(new Product("Macbook pro", "Laptop", "Brown Drawer", 2022));
-//        service.addProduct(new Product("Wacom Pad", "Writing Pad", "Black Drawer", 2020));
-//        service.addProduct(new Product("Apple Keyboard", "Keyboard", "White Table", 2022));
-//        service.addProduct(new Product("Logitech Keyboard", "Keyboard", "Black Table", 2024));
-//        service.addProduct(new Product("Hdmi cable", "Cable", "Black Drawer", 2024));
-//        service.addProduct(new Product("Java Black Book", "Cable", "Shelf", 2024));
-//        service.addProduct(new Product("Logi Mouse", "Mouse", "Black Table", 2022));
-//        service.addProduct(new Product("Apple Mouse", "Mouse", "White Table", 2022));
-//        service.addProduct(new Product("Lenovo Mouse", "Mouse", "Black Drawer", 2022));
-//        service.addProduct(new Product("BlackBeast", "Computer", "White Table", 2020));
+        Product p=new Product("Asus","Laptop","Brown",2000);
+        service.addProduct(p);
+        service.addProduct(new Product("Type C", "Cable", "Black Drawer", 2024));
+        service.addProduct(new Product("Mac Studio", "Computer", "White Table", 2025));
+        service.addProduct(new Product("Focusrite Mixer", "Audio System", "White Table", 2025));
+        service.addProduct(new Product("Asus Vivobook", "Laptop", "Brown Drawer", 2021));
+        service.addProduct(new Product("Asus Rog", "Laptop", "Black Table", 2021));
+        service.addProduct(new Product("Macbook pro", "Laptop", "Brown Drawer", 2022));
+        service.addProduct(new Product("Wacom Pad", "Writing Pad", "Black Drawer", 2020));
+        service.addProduct(new Product("Apple Keyboard", "Keyboard", "White Table", 2022));
+        service.addProduct(new Product("Logitech Keyboard", "Keyboard", "Black Table", 2024));
+        service.addProduct(new Product("Hdmi cable", "Cable", "Black Drawer", 2024));
+        service.addProduct(new Product("Java Black Book", "Cable", "Shelf", 2024));
+        service.addProduct(new Product("Logi Mouse", "Mouse", "Black Table", 2022));
+        service.addProduct(new Product("Apple Mouse", "Mouse", "White Table", 2022));
+        service.addProduct(new Product("Lenovo Mouse", "Mouse", "Black Drawer", 2022));
+        service.addProduct(new Product("BlackBeast", "Computer", "White Table", 2020));
 
         List<Product> products=service.getAllProduct();
         for(Product pd:products){
--- ProductSpringWeb/src/main/java/com/example/ProductSpringWeb/ProductController.java
+++ ProductSpringWeb/src/main/java/com/example/ProductSpringWeb/ProductController.java
@@ -1,6 +1,28 @@
 package com.example.ProductSpringWeb;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 public class ProductController {
 
-}
+    @Autowired
+    ProductService service;
+
+    @GetMapping("/products")
+    public List<Product> getAllProduct(){
+        return service.getAllProduct();
+    }
+
+    @GetMapping("/products/{name}")
+    public Product getProduct(@PathVariable  String name){
+        return service.getProduct(name);
+    }
+
+    @PostMapping("/product")
+    public void addProduct(@RequestBody Product p){
+        service.addProduct(p);
+    }
+}   
